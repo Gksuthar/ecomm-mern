15cc05c55bcd73f7f68366825cea5d2cb8785278
Your Name <your_email@example.com>
2025-03-13 11:52:12 +0530

    set icons

--- src/components/Navigation/MobileNav.jsx
+++ src/components/Navigation/MobileNav.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import { IoHomeOutline } from "react-icons/io5";
 import { IoSearchOutline } from "react-icons/io5";
 import { CiHeart } from "react-icons/ci";
+import { IoBagOutline } from "react-icons/io5";
+import { RiAccountBox2Line } from "react-icons/ri";
 
 const MobileNav = () => {
   return (
-    <div className="mobileNav fixed bottom-0 left-0 p-1 px-2  w-full bg-white z-[999] shadow-md border-t">
+    <div className="mobileNav fixed bottom-0 left-0 p-1 px-2  w-full bg-white z-[999] shadow-md border-t flex justify-between">
       <Button
         className="flex flex-col items-center !p-0 !w-[50px] !min-w-[50px] !capitalize !text-gray-600"
         disableRipple
@@ -32,14 +34,14 @@ const MobileNav = () => {
         className="flex flex-col items-center !p-0 !w-[50px] !min-w-[50px] !capitalize !text-gray-600"
         disableRipple
       >
-        <IoHomeOutline size={20} className="mb-1" />
+        <IoBagOutline size={20} className="mb-1" />
         <span className="text-[14px] leading-none">Orders</span>
       </Button>
       <Button
         className="flex flex-col items-center !p-0 !w-[50px] !min-w-[50px] !capitalize !text-gray-600"
         disableRipple
       >
-        <IoHomeOutline size={20} className="mb-1" />
+        <RiAccountBox2Line size={20} className="mb-1" />
         <span className="text-[14px] leading-none">Account</span>
       </Button>
     </div>
